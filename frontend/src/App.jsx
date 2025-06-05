84b28e7c3681a49d312012ec5f396b4b754c810e
CHubert <66801304+chubert688@users.noreply.github.com>
2025-06-05 15:46:34 -0300

    Update build
    Finalizada a implementação do projeto seguindo vídeo tutorial

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
-import './App.css'
+import './css/App.css'
+import Favorites from './pages/Favorites'
 import Home from './pages/Home'
+import NavBar from './components/NavBar'
+import { Routes, Route } from 'react-router-dom'
+import { MovieProvider } from './contexts/MovieContext'
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <MovieProvider>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/favorites" element={<Favorites />}/>
+        </Routes>
+      </main>
+    </MovieProvider>
   )
 }
 
---
+++ frontend/src/services/api.js
@@ -0,0 +1,15 @@
+const API_KEY = "2c9515b33242a3c320ef8d0f33a61605"
+const BASE_URL = "https://api.themoviedb.org/3"
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`)
+  const data = await response.json()
+  return data.results
+}
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`)
+  const data = await response.json()
+  return data.results
+}
